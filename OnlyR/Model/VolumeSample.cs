d11c3281eb85d9eebfcdbbd60cc81ebb8169612b
Jose Hernandez <jose@josehernandez.com>
2020-01-24 10:41:06 -0500

    Added missing VolumeSample class to Git. (Did not get checked in by VS??)

---
+++ OnlyR/Model/VolumeSample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyR.Model
+{
+    public class VolumeSample
+    {
+        public int VolumeLevel { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
