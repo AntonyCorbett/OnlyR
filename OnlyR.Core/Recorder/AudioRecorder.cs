0b1859b936c3f3d0db8cbf4e86d37d10a3387e5f
AntonyCorbett <antony@corbetts.org.uk>
2021-07-03 16:23:39 +0100

    Revert "silence provider for loopback capture"
    This reverts commit 7abae0a5e81ceee0e149aacb8114cdc50f49f308.

--- OnlyR.Core/Recorder/AudioRecorder.cs
+++ OnlyR.Core/Recorder/AudioRecorder.cs
@@ -22,7 +22,6 @@ namespace OnlyR.Core.Recorder
 
         private LameMP3FileWriter? _mp3Writer;
         private IWaveIn? _waveSource;
-        private WaveOutEvent? _silenceWaveOut;
         private SampleAggregator? _sampleAggregator;
         private VolumeFader? _fader;
         private RecordingStatus _recordingStatus;
@@ -74,7 +73,6 @@ namespace OnlyR.Core.Recorder
                 if (recordingConfig.UseLoopbackCapture)
                 {
                     _waveSource = new WasapiLoopbackCapture();
-                    ConfigureSilenceOut();
                 }
                 else
                 {
@@ -98,20 +96,8 @@ namespace OnlyR.Core.Recorder
                     CreateTag(recordingConfig));
 
                 _waveSource.StartRecording();
-                
                 OnRecordingStatusChangeEvent(new RecordingStatusChangeEventArgs(RecordingStatus.Recording));
             }
-        }
-
-        private void ConfigureSilenceOut()
-        {
-            // WasapiLoopbackCapture doesn't record any audio when nothing is playing
-            // so we must play some silence!
-
-            var silence = new SilenceProvider(new WaveFormat(44100, 2));
-            _silenceWaveOut = new WaveOutEvent();
-            _silenceWaveOut.Init(silence);
-            _silenceWaveOut.Play();
         }
 
         /// <summary>
@@ -131,7 +117,6 @@ namespace OnlyR.Core.Recorder
                 else
                 {
                     _waveSource?.StopRecording();
-                    _silenceWaveOut?.Stop();
                 }
             }
         }
@@ -266,7 +251,6 @@ namespace OnlyR.Core.Recorder
         private void FadeCompleteHandler(object? sender, System.EventArgs e)
         {
             _waveSource?.StopRecording();
-            _silenceWaveOut?.Stop();
         }
 
         private void Cleanup()
@@ -275,9 +259,6 @@ namespace OnlyR.Core.Recorder
 
             _waveSource?.Dispose();
             _waveSource = null;
-
-            _silenceWaveOut?.Dispose();
-            _silenceWaveOut = null;
 
             _mp3Writer?.Dispose();
             _mp3Writer = null;
