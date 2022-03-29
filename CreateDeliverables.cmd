d1ba7ae5381cc41f91b30d87a2d101430dfbcf98
Antony Corbett <antony@corbetts.org.uk>
2022-03-29 18:29:39 +0100

    Updated build pipeline

--- CreateDeliverables.cmd
+++ CreateDeliverables.cmd
@@ -1,17 +1,35 @@
 REM Run from dev command line
+
+@ECHO OFF
+
+VERIFY ON
+
 D:
 cd \ProjectsPersonal\OnlyR
 rd OnlyR\bin /q /s
 rd Installer\Output /q /s
 
-REM build / publish
+ECHO.
+ECHO Publishing OnlyR
 dotnet publish OnlyR\OnlyR.csproj -p:PublishProfile=FolderProfile -c:Release
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM delete unwanted NAudio x64
-del OnlyR\bin\Release\net5.0-windows\publish\libmp3lame.64.dll
+ECHO.
+ECHO Creating installer
+"D:\Program Files (x86)\Inno Setup 6\iscc" Installer\onlyrsetup.iss
+IF %ERRORLEVEL% NEQ 0 goto ERROR
 
-REM Create installer
-"C:\Program Files (x86)\Inno Setup 6\iscc" Installer\onlyrsetup.iss
-
-REM create portable zip
+ECHO.
+ECHO Creating portable zip
 powershell Compress-Archive -Path OnlyR\bin\Release\net5.0-windows\publish\* -DestinationPath Installer\Output\OnlyRPortable.zip 
+IF %ERRORLEVEL% NEQ 0 goto ERROR
+
+goto SUCCESS
+
+:ERROR
+ECHO.
+ECHO ******************
+ECHO An ERROR occurred!
+ECHO ******************
+
+:SUCCESS
