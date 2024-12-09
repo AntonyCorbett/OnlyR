4d89a0cffc259bd56cd483847065103dd70c4afe
WailGree <szilagyi.roland.dominik@gmail.com>
2024-12-09 19:49:10 +0100

    Merge mp3Writer and wavFileWriter into single stream instance

--- OnlyR.Core/Recorder/AudioRecorder.cs
+++ OnlyR.Core/Recorder/AudioRecorder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using NAudio.Lame;
 using NAudio.Wave;
 using OnlyR.Core.Enums;
@@ -20,8 +21,7 @@ namespace OnlyR.Core.Recorder
         private const int RequiredReportingIntervalMs = 40;
         private const int VuSpeed = 5;
 
-        private LameMP3FileWriter? _mp3Writer;
-        private WaveFileWriter? _waveFileWriter;
+        private Stream? _audioWriter;
         private IWaveIn? _waveSource;
         private WaveOutEvent? _silenceWaveOut;
         private SampleAggregator? _sampleAggregator;
@@ -94,21 +94,16 @@ namespace OnlyR.Core.Recorder
                 _waveSource.DataAvailable += WaveSourceDataAvailableHandler;
                 _waveSource.RecordingStopped += WaveSourceRecordingStoppedHandler;
 
-                switch (recordingConfig.Codec)
+                _audioWriter = recordingConfig.Codec switch
                 {
-                    case AudioCodec.Mp3:
-                        _mp3Writer = new LameMP3FileWriter(
-                            recordingConfig.DestFilePath,
-                            _waveSource.WaveFormat,
-                            recordingConfig.Mp3BitRate!.Value,
-                            CreateTag(recordingConfig));
-                        break;
-                    case AudioCodec.Wav:
-                        _waveFileWriter = new WaveFileWriter(recordingConfig.DestFilePath, _waveSource.WaveFormat);
-                        break;
-                    default:
-                        throw new NotSupportedException("Unsupported codec");
-                }
+                    AudioCodec.Mp3 => new LameMP3FileWriter(
+                        recordingConfig.DestFilePath,
+                        _waveSource.WaveFormat,
+                        recordingConfig.Mp3BitRate!.Value,
+                        CreateTag(recordingConfig)),
+                    AudioCodec.Wav => new WaveFileWriter(recordingConfig.DestFilePath, _waveSource.WaveFormat),
+                    _ => throw new NotSupportedException("Unsupported codec"),
+                };
 
                 _waveSource.StartRecording();
 
@@ -234,8 +229,7 @@ namespace OnlyR.Core.Recorder
 
             AddToSampleAggregator(buffer, bytesRecorded, isFloatingPointAudio);
 
-            _waveFileWriter?.Write(buffer, 0, bytesRecorded);
-            _mp3Writer?.Write(buffer, 0, bytesRecorded);
+            _audioWriter?.Write(buffer, 0, bytesRecorded);
         }
 
         private void AddToSampleAggregator(byte[] buffer, int bytesRecorded, bool isFloatingPointAudio)
@@ -296,8 +290,7 @@ namespace OnlyR.Core.Recorder
 
         private void Cleanup()
         {
-            _mp3Writer?.Flush();
-            _waveFileWriter?.Flush();
+            _audioWriter?.Flush();
 
             _waveSource?.Dispose();
             _waveSource = null;
@@ -305,11 +298,8 @@ namespace OnlyR.Core.Recorder
             _silenceWaveOut?.Dispose();
             _silenceWaveOut = null;
 
-            _mp3Writer?.Dispose();
-            _mp3Writer = null;
-
-            _waveFileWriter?.Dispose();
-            _waveFileWriter = null;
+            _audioWriter?.Dispose();
+            _audioWriter = null;
 
             _tempRecordingFilePath = null;
         }
